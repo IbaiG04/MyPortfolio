120042ecb798711bdc83d9916ebb878bedb3e979
IbaiG04 <ibaigm04@gmail.com>
2024-03-21 14:31:32 +0100

    cardUpdate

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,33 @@
     <div class="relative z-10">
       <header class="flex items-center justify-center">
         <nav class="text-white">
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/info">CardInfo</RouterLink>
-
+         
+          <router-link to="/" class="text-white">Home</router-link>
+          <router-link to="/about" class="text-white">About</router-link>
+          <router-link to="/projects" class="text-white">Projects</router-link>
+          <router-link to="/contact" class="text-white">Contact</router-link>
         </nav>
       </header>
       <body>
         
+        
+        
         <RouterView class="relative z-10"/>
+        <cardInfoVue/> 
       </body>
     </div>
   </div>
 </template>
 
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
+import { RouterView } from 'vue-router'
+import cardInfoVue from './components/cardInfo.vue'
+
+export default {
+ name: 'App',
+ components: {
+  RouterView,
+  cardInfoVue,
+ }
+}
 </script>
--- src/components/cardInfo.vue
+++ src/components/cardInfo.vue
@@ -1,8 +1,82 @@
 <template>
-    <div>
-        
-</div>
+  <div class="text-white flex items-center justify-center flex-col pt-28">
+    <h2 class="text-4xl">About me</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4  p-10 ">
+
+      <div @click="handleClick" 
+      @mouseover="isHovered = true" 
+      @mouseleave="isHovered = false"
+        :class="{ 'cursor-pointer': isHovered }"
+        class=" hover:scale-105  transition-transform block rounded-xl border bg-black bg-opacity-50 border-gray-800 p-4">
+
+        <span class="inline-block rounded-lg p-3">
+          <div class="inline-flex align-middle justify-center items-center select-none text-white">
+            <svg class="" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg" height="32" width="32">
+              <path
+                d="M8.50141 14.75C8.3114 14.75 8.12141 14.68 7.97141 14.53L5.97141 12.53C5.68141 12.24 5.68141 11.7599 5.97141 11.4699L7.97141 9.46994C8.26141 9.17994 8.7414 9.17994 9.0314 9.46994C9.3214 9.75994 9.3214 10.24 9.0314 10.53L7.5614 12L9.0314 13.4699C9.3214 13.7599 9.3214 14.24 9.0314 14.53C8.8814 14.68 8.69141 14.75 8.50141 14.75Z"
+                fill="currentColor"></path>
+              <path
+                d="M16.5014 14.75C16.3114 14.75 16.1214 14.68 15.9714 14.53C15.6814 14.24 15.6814 13.7599 15.9714 13.4699L17.4414 12L15.9714 10.53C15.6814 10.24 15.6814 9.75994 15.9714 9.46994C16.2614 9.17994 16.7414 9.17994 17.0314 9.46994L19.0314 11.4699C19.3214 11.7599 19.3214 12.24 19.0314 12.53L17.0314 14.53C16.8814 14.68 16.6914 14.75 16.5014 14.75Z"
+                fill="currentColor"></path>
+              <path
+                d="M12.5 22.75C6.57 22.75 1.75 17.93 1.75 12C1.75 6.07 6.57 1.25 12.5 1.25C18.43 1.25 23.25 6.07 23.25 12C23.25 17.93 18.43 22.75 12.5 22.75ZM12.5 2.75C7.4 2.75 3.25 6.9 3.25 12C3.25 17.1 7.4 21.25 12.5 21.25C17.6 21.25 21.75 17.1 21.75 12C21.75 6.9 17.6 2.75 12.5 2.75Z"
+                fill="currentColor"></path>
+              <path
+                d="M11.5021 15.0801C11.4021 15.0801 11.3021 15.0601 11.2121 15.0201C10.8321 14.8601 10.6521 14.4201 10.8221 14.0301L12.8221 9.36004C12.9821 8.98004 13.4221 8.80002 13.8021 8.97002C14.1821 9.13002 14.3621 9.57008 14.1921 9.96008L12.1921 14.6301C12.0721 14.9101 11.7921 15.0801 11.5021 15.0801Z"
+                fill="currentColor"></path>
+            </svg>
+          </div>
+        </span>
+
+        <h2 class="mt-2 font-semibold text-base sm:text-lg text-white">
+          About
+        </h2>
+        <p class="sm:mt-1 block text-sm sm:text-base text-gray-300"> Frontend developer junior apasionado por crear
+          experiencias digitales impactantes con un enfoque centrado en el usuario.
+        </p>
+
+      </div>
+
+      <div
+      @click="handleClick" 
+      @mouseover="isHovered = true" 
+      @mouseleave="isHovered = false"
+        :class="{ 'cursor-pointer': isHovered }"
+        class=" hover:scale-105  transition-transform block rounded-xl border bg-black bg-opacity-50 border-gray-800 p-4">
+        <span class="inline-block rounded-lg p-3">
+          <div class="inline-flex align-middle justify-center items-center select-none text-white">
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" height="32" width="32">
+              <path
+                d="M7.55556 4.22222C7.55556 2.99492 8.55048 2 9.77778 2C11.0051 2 12 2.99492 12 4.22222V5.44444C12 5.99673 12.4477 6.44444 13 6.44444H16.5556C17.1078 6.44444 17.5556 6.89216 17.5556 7.44444V11C17.5556 11.5523 18.0033 12 18.5556 12H19.7778C21.0051 12 22 12.9949 22 14.2222C22 15.4496 21.0051 16.4444 19.7778 16.4444H18.5556C18.0033 16.4444 17.5556 16.8922 17.5556 17.4444V21C17.5556 21.5523 17.1078 22 16.5556 22H13C12.4477 22 12 21.5523 12 21V19.7778C12 18.5504 11.0051 17.5556 9.77778 17.5556C8.55048 17.5556 7.55556 18.5504 7.55556 19.7778V21C7.55556 21.5523 7.10784 22 6.55556 22H3C2.44772 22 2 21.5523 2 21V17.4444C2 16.8922 2.44772 16.4444 3 16.4444H4.22222C5.44952 16.4444 6.44444 15.4496 6.44444 14.2222C6.44444 12.9949 5.44952 12 4.22222 12H3C2.44772 12 2 11.5523 2 11V7.44444C2 6.89216 2.44772 6.44444 3 6.44444H6.55556C7.10784 6.44444 7.55556 5.99673 7.55556 5.44444V4.22222Z"
+                stroke="currentColor" stroke-width="1.5" stroke-linecap="square"></path>
+            </svg>
+          </div>
+        </span>
+        <h2 class="mt-2 font-semibold text-base sm:text-lg text-white">
+          Projects
+        </h2>
+        <p class="sm:mt-1 block text-sm sm:text-base text-gray-300"> Ávido aprendiz y apasionado por mantenerse al día
+          con las últimas tendencias tecnológicas y mejores prácticas de desarrollo, comprometido con el crecimiento
+          profesional continuo y la mejora constante en el campo de la programación web.
+        </p>
+      </div>
+
+    </div>
+  </div>
 </template>
-<h1>Hola</h1>
 <script>
+export default {
+  name: 'cardInfo',
+  data() {
+    return {
+      isHovered: false
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log('El div fue clickeado')
+    }
+  }
+}
+
 </script>
